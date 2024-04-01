066f58190d805982bf03a7c639293e1d3ff1dba3
renanMeloDev <dev.renanmelo@gmail.com>
2024-04-01 16:41:35 -0300

    desafio felião-calculadora de partida rankiadas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-let userName = getFirtsName ("Renan José Santos de Melo")
-console.log ("Seja Bem-vindo"+ userName)
+let heroName =("Sagaz")
+let resultado = vitoryDefeat( )
+console.log(heroName + " teve um total de vitorias de " + resultado)
 
-function getFirtsName (name){
-    let firtsName= name.split (" ")[0]
-    return firtsName
-
+function vitoryDefeat (point){
+    let vitory = 129
+    let defeat = 78
+    point = (vitory - defeat)
+    return point
+    console.log(point)
+}
+if (resultado < 10){ 
+	console.log (heroName + " tem nível ferro")
+}else if (resultado <=11){
+	console.log(heroName + " tem nível bronze")
+}else if (resultado < 20){
+	console.log(heroName + "tem nível bronze")
+}else if(resultado <=21){
+	console.log(heroName+" tem nível prata")
+}else if (resultado <50){
+	console.log(heroName + " tem nivel prata")
+}else if (resultado<=51){
+	console.log(heroName + " tem nível ouro")
+}else if (resultado<80){
+	console.log(heroName + " tem nível ouro")
+}else if (resultado <=81){
+	console.log(heroName + " tem nível Diamante")
+}else if (resultado<90) {
+	console.log(heroName + " tem nível Dimante")
+}else if(resultado<=91){
+	console.log(heroName + " tem nível léndario")
+}else if (resultado >=101) {
+	console.log(heroName + " tem nível Imortal")
 }
