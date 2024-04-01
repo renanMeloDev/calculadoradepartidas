2057118079146a00dccfb6dfc4a7dbeebfd4b445
renanMeloDev <dev.renanmelo@gmail.com>
2024-04-01 16:40:05 -0300

    desafio felião-calculadora de partida rankiadas

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let userName = getFirtsName ("Renan José Santos de Melo")
+console.log ("Seja Bem-vindo"+ userName)
+
+function getFirtsName (name){
+    let firtsName= name.split (" ")[0]
+    return firtsName
+
+}
